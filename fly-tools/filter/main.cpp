230c1ed46fdc851239647731cddd9a3dc3634fa9
mutantturkey <mutantturke@gmail.com>
2012-08-02 19:15:10 -0400

    employ logging system, fixup output to be more consistent, get rid of returned result

--- fly-tools/filter/main.cpp
+++ fly-tools/filter/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <cvblob.h>
 
 using namespace std;
 using namespace cvb;
+
+ofstream nullLog;
+ostream* output;
 
 bool cmpArea(const pair<CvLabel, CvBlob*>  &p1, const pair<CvLabel, CvBlob*> &p2)
 {
@@ -16,13 +21,13 @@ int main(int argc, char* argv[]) {
   int c;
   int drawNumber = 2;
 
-  double ratio;
+  double ratio = 0;
 
-  unsigned int result;
-
-  bool ratioSet;
+  bool verbose = false;
+  bool ratioSet = false;
 
   string usage = "filter-mask -i <input-file> -o <output-file> -r <ratio>";
+
   string inputFileName;
   string outputFileName;
 
@@ -36,13 +41,16 @@ int main(int argc, char* argv[]) {
 
   vector< pair<CvLabel, CvBlob*> > blobList;
 
-  while ((c = getopt (argc, argv, "i:o:r:h")) != -1)
+  while ((c = getopt (argc, argv, "i:o:r:hv")) != -1)
     switch (c) {
       case 'i':
         inputFileName = optarg;
         break;
       case 'o':
         outputFileName = optarg;
+        break;
+      case 'v':
+        verbose = true;
         break;
       case 'r':
         ratioSet = true;
@@ -61,15 +69,25 @@ int main(int argc, char* argv[]) {
     exit(1);
   }	
 
-  img = cvLoadImage(inputFileName.c_str(), 1);
-  imgOut = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1); cvZero(imgOut);
+  if(verbose) {
+    output = &cout;
+  }	else {
+    output = &nullLog; 
+  }
 
+  *output << "Verbose logging enabled" << endl;
+
+  // read input file
+  img = cvLoadImage(inputFileName.c_str(), 1);
+
+  imgOut = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1); cvZero(imgOut);
   grey = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1);
 
   cvCvtColor(img, grey, CV_BGR2GRAY);
 
+  // label blobs
   labelImg = cvCreateImage(cvGetSize(grey),IPL_DEPTH_LABEL,1);
-  result = cvLabel(grey, labelImg, blobs);
+  cvLabel(grey, labelImg, blobs);
 
   // copy and sort blobs
   copy(blobs.begin(), blobs.end(), back_inserter(blobList));
@@ -77,26 +95,26 @@ int main(int argc, char* argv[]) {
 
   // if no blobs.
   if(blobList.size() == 0) { 
-    cerr << "No blobs found." << endl;
+    cerr << "No blobs found" << endl;
     cvSaveImage(outputFileName.c_str(), imgOut);
     exit(1);
   } 
 
   // if only one blob is detected.
   if(blobList.size() == 1 ) {
-    cout << "Only one blob found!" << endl;
+    *output << "Only one blob found" << endl;
     drawNumber = 1;
   }
 
   // if the ratio of the of the two blobs is smaller than the input ratio.
   if( ((double)blobList[blobList.size()-drawNumber].second->area / (double)blobList[blobList.size()-1].second->area) < (1/ratio) ) {
-    cout << "the second largest blob is smaller than the ratio. only drawing largest blob!" << endl;
+    *output << "the second largest blob is smaller than the ratio. only drawing largest blob" << endl;
     drawNumber = 1; 
   }
 
-  for (int i=blobList.size()-drawNumber; i<blobList.size(); i++) {
+  for (int i=blobList.size()-drawNumber; i<(int)blobList.size(); i++) {
     largeBlobs.insert( CvLabelBlob(blobList[i].first, blobList[i].second) );
-    cout << "Blob #" << blobList[i].first << " -> " << (*blobList[i].second) << endl;
+    *output << "Blob #" << blobList[i].first << " -> " << (*blobList[i].second) << endl;
   }
 
   // draw the selected blobsto imgOut and write the image.
