4b3f87cc931202d1948eef870bfb12d0349fa6f4
mutantturkey <mutantturke@gmail.com>
2012-09-24 16:50:48 -0400

    MyPair.h line cleanups and formatting changes

--- fly-tools/MyPair.h
+++ fly-tools/MyPair.h
@@ -1,30 +1,31 @@
 class MyPair {
 
-public:
-	int first, second;
-	MyPair(int x, int y) {
-	
-		this->first = x;
-		this->second = y;
-		
-	}
-	MyPair() {
-		
-		this->first = -1;
-		this->second = -1;
-		
-	}
-	MyPair(const MyPair &f) {
-		this->first = f.getX();
-		this->second = f.getY();
-		
-	}
-	int getX() const {
-		return this->first;
-	}
+  public:
 
-	int getY() const {
-		return this->second;
-	}
-	
+    int first;
+    int second;
+
+    MyPair(int x, int y) {
+      this->first = x;
+      this->second = y;
+    }
+
+    MyPair() {
+      this->first = -1;
+      this->second = -1;
+    }
+
+    MyPair(const MyPair &f) {
+      this->first = f.getX();
+      this->second = f.getY();
+    }
+
+    int getX() const {
+      return this->first;
+    }
+
+    int getY() const {
+      return this->second;
+    }
+
 };
