e2cd8c01425c08e07d4ad6c6ed0a8535c46a9ae5
mutantturkey <mutantturke@gmail.com>
2012-10-24 10:06:35 -0400

    remove unused, commented, functions

--- fly-tools/FlyObject.h
+++ fly-tools/FlyObject.h
@@ -1,6 +1,5 @@
 /*
  *  FlyObject.h
- *  
  *
  *  Created by Md. Alimoor Reza on 6/26/10.
  *  Copyright 2010 Drexel University. All rights reserved.
@@ -14,7 +13,6 @@ using namespace std;
 
 class FlyObject {
 public:
-	//FlyObject(int area, pair<int, int> centroid, pair<double,double> majorAxisEV, pair<double,double> velocityV, vector<pair<int, int> > areaCoord);
 	FlyObject(int area, pair<int, int> centroid, pair<double,double> majorAxisEV, pair<double,double> velocityV,bool headIsInDirectionMAEV, pair<double, double> head, double speed);
 	FlyObject(const FlyObject &f);
 	int getArea() const;
@@ -24,7 +22,6 @@ public:
 	bool getHeadIsInDirectionMAEV() const;
 	pair<double,double> getHead() const;
 	double getSpeed() const;
-	//vector<pair<int, int> > getAreaCoord() const;
 	void setArea(int area);
 	void setCentroid(pair<int, int>);
 	void setMajorAxisEV(pair<double,double>);
@@ -33,13 +30,11 @@ public:
 	void setHeadIsInDirectionMAEV(bool);
 	void setHead(pair<double, double> head);
 	void setSpeed(double speed);
-	//void setAreaCoord(vector<pair<int , int> >);
 	void output(ostream &out);
 	
 
 private:
 	int area;
-	//vector<pair<int , int> > areaCoord;
 	pair<int, int> centroid; 
 	pair<double,double> majorAxisEV;
 	pair<double,double> velocityV;
